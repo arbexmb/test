b2d8a7f4129edda11cd491569e53aeb41c5260ce
arbexmb <lucasarbexmb@gmail.com>
2022-02-21 15:05:07 -0300

    feat: teste

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const github = require('@actions/github');
 
 try {
     const input = core.getInput('input-teste')
-    console.log(input)
+    console.log({ input })
     const output = core.setOutput('output-teste', 'skore')
-    console.log(output)
+    console.log({ output })
 } catch (error) {
     core.setFailed(error.message);
 }
